4df4923091df6608c24a985b526c5d41beb0e377
q8iqbal <49096237+q8iqbal@users.noreply.github.com>
2021-11-23 16:51:47 +0700

    tes

--- tes.py
+++ tes.py
@@ -3,17 +3,18 @@ import sys
 try:
     from mfrc522 import SimpleMFRC522
     import RPi.GPIO as GPIO
-except (ImportError, ModuleNotFoundError):
+except ImportError:
     from dummy_package.mfrc522 import SimpleMFRC522
     import RPiSim.GPIO as GPIO
 
-clreader = SimpleMFRC522()
+reader = SimpleMFRC522()
 
 try:
     while True:
         print("Hold a tag near the reader")
-        id, text = reader.read()
-        print("ID: %s\nText: %s" % (id,text))
+        tes = reader.read()
+#         print("ID: %s\nText: %s" % (id,text))
+        print(tes)
         sleep(5)
 except KeyboardInterrupt:
     GPIO.cleanup()
